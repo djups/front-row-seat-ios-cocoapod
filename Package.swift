1c8f92069d6e6df41ea19cad68d4576a2dcee64e
Johan Lajili <johan.lajili@imgarena.com>
2023-02-17 13:57:52 +0000

    wip: trying to fix automated tag release

--- Package.swift
+++ Package.swift
@@ -9,20 +9,17 @@ let package = Package(
         .iOS(.v12),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "IMGA",
             targets: ["IMGA"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-url: "https://github.com/IMGARENA/front-row-seat-ios-cocoapod/releases/download/v0.8/IMGA.xcframework.zip"
     ],
     targets: [
         .binaryTarget(
             name: "IMGA",
-url: "https://github.com/IMGARENA/front-row-seat-ios-cocoapod/releases/download/v0.8/IMGA.xcframework.zip"
-checksum: "ee1d0d2ac8188739aff8abcf46177415bfdaf925da7b6d4448ed4affb2e383e4"
+            url: "https://github.com/IMGARENA/front-row-seat-ios-cocoapod/releases/download/v0.7/IMGA.xcframework.zip",
+            checksum: "ee1d0d2ac8188739aff8abcf46177415bfdaf925da7b6d4448ed4affb2e383e4"
         )
     ]
 )
